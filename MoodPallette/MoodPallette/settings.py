d1d54be8a16267e3d408957839cbac5a28b6c1ac
una_ah <kye011214@naver.com>
2022-07-22 18:45:18 +0900

    feat: add account app

--- MoodPallette/MoodPallette/settings.py
+++ MoodPallette/MoodPallette/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'account',
     'sbti',
 ]
@@ -45,7 +46,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+ #   'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -123,3 +124,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# 유저모델 변경
+AUTH_USER_MODEL = 'account.User'
+
+# "detail": "CSRF Failed: CSRF cookie not set." 오류 해결
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
--- MoodPallette/account/views.py
+++ MoodPallette/account/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import auth
+from django.contrib.auth import login, authenticate
+from .models import UserManager, User
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.exceptions import ValidationError
 
-# Create your views here.
+class login(APIView):
+    def post(self, request, *args, **kwargs):
+        userEmail = request.data["email"]
+        pwd = request.data["password"]
+
+        user = auth.authenticate(email=userEmail, password=pwd)
+
+        # 유저 정보 확인
+        if user is not None:
+            auth.login(request, user)
+            return Response({"message": "Success Login"}, status=200)
+        
+        # 가입하지 않은 유저인 경우
+        else:
+            return Response({"message": "유저 정보가 없음"}, status=404)
+
+# 로그아웃 이게 맞나??????
+def logout(request):
+    auth.logout(request)
+    return Response({"message": "Success Logout"}, status=200)
+
+class signup(APIView):
+    def post(self, request, *args, **kwargs):
+
+        # 이미 가입한 email인 경우
+        if User.objects.filter(email=request.data['email']):
+            return Response({"message": "이미 있는 email"}, status=409)
+        
+        # password1이랑 password2가 일치
+        elif request.data["password1"] == request.data["password2"]:
+            user = User.objects.create_user(
+                email = request.data["email"],
+                nickname = request.data["nickname"],
+                password = request.data["password1"]
+            )
+            #auth.login(request, user)   
+            return Response({"message": "Success signup"}, status=201)
+
+        # 불일치
+        elif request.data["password1"] != request.data["password2"]:
+            return Response({"message": "password가 일치하지 않음!!"}, status=400)
+    
