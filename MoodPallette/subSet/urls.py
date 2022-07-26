e27791b3fa1ec90610b7b88923c4a32ef33b133e
dd <12202340@inha.edu>
2022-07-26 21:10:37 +0900

    feat: add subSet

--- MoodPallette/subSet/urls.py
+++ MoodPallette/subSet/urls.py
@@ -4,8 +4,6 @@ from subSet import views
 from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
-    path('<int:userId>/create/', csrf_exempt(views.subSet_API.as_view())),
-    path('<int:userId>/', csrf_exempt(views.subSet_API.as_view())),
-    path('<int:userId>/update', csrf_exempt(views.subSet_API.as_view())),
+    path('<int:userId>/', csrf_exempt(views.subSet_API.as_view()))
 ]
 
