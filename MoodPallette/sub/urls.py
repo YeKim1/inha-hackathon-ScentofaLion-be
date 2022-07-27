c86ceea54c1e1262324cbcd8110901f20709ab11
dd <12202340@inha.edu>
2022-07-27 13:19:32 +0900

    feat: add sub

--- MoodPallette/sub/urls.py
+++ MoodPallette/sub/urls.py
@@ -6,5 +6,5 @@ from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path('<int:userId>/', csrf_exempt(views.subSet_API.as_view())),
-    path('<int:userId>/count/', csrf_exempt(views.subSet_TF.as_view())),
+    path('<int:userId>/product/', csrf_exempt(views.sub_product.as_view())),
 ]
